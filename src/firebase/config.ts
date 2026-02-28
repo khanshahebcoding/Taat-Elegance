2a730dd8edb2a31f2cf5e871f79ba11902de7d45
Mohammad Sajjad Khan <petscare24org@gmail.com>
2026-02-28 08:39:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9057947856-bb535",
+  "appId": "1:1065139326393:web:53c6c29cdcbe77cd9b3a68",
+  "apiKey": "AIzaSyB64U4Zfeta2mfKztyHMNWby844_sl9-6I",
+  "authDomain": "studio-9057947856-bb535.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1065139326393"
+};
